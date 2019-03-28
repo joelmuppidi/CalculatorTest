5b8eeef11266d478006b1643be241b75c2718522
joelmuppidi <43320670+joelmuppidi@users.noreply.github.com>
2019-03-28 12:24:23 +0530

    Update calc.c
    test build

--- calc.c
+++ calc.c
@@ -2,37 +2,45 @@
 
 # include <stdio.h>
 
+double summation(double x,double y);
+double subtraction(double x,double y);
+double multiplication(double x,double y);
+double division(double x,double y);
+
 int main() {
 
     char ope;
     double firstNumber,secondNumber;
 
     printf("Enter an operator (+, -, *, /): ");
-    scanf("%c", &ope);
+    //scanf("%c", &ope);
+    ope='*';
 
     printf("Enter first operand: ");
-    scanf("%lf",&firstNumber);
+    //scanf("%lf",&firstNumber);
+    firstNumber=10;
 
     printf("Enter second operand: ");
-    scanf("%lf",&secondNumber);
+   //scanf("%lf",&secondNumber);
+   secondNumber=4;
 
     switch(ope)
     {
         case '+':
-            printf("%.1lf + %.1lf = %.1lf",firstNumber, secondNumber, firstNumber + secondNumber);
+            printf("%.1lf + %.1lf = %.1lf",firstNumber, secondNumber, summation(firstNumber,secondNumber));
             break;
 
         case '-':
-            printf("%.1lf - %.1lf = %.1lf",firstNumber, secondNumber, firstNumber - secondNumber);
+            printf("%.1lf - %.1lf = %.1lf",firstNumber, secondNumber, subtraction(firstNumber,secondNumber));
             break;
 
         case '*':
-            printf("%.1lf * %.1lf = %.1lf",firstNumber, secondNumber, firstNumber * secondNumber);
+            printf("%.1lf * %.1lf = %.1lf",firstNumber, secondNumber, multiplication(firstNumber,secondNumber));
             break;
 
         case '/':
             if(secondNumber)
-                printf("%.1lf / %.1lf = %.1lf",firstNumber, secondNumber, firstNumber / secondNumber);
+                printf("%.1lf / %.1lf = %.1lf",firstNumber, secondNumber, division(firstNumber,secondNumber));
             else
                 printf("undefined");
             break;
@@ -42,5 +50,23 @@ int main() {
             printf("Error! operator is not correct");
     }
 
+
     return 0;
+}
+
+
+double summation(double x,double y){
+    return(x+y);
+}
+
+double subtraction(double x,double y){
+    return(x-y);
+}
+
+double multiplication(double x,double y){
+    return(x*y);
+}
+
+double division(double x,double y){
+    return(x/y);
 }
